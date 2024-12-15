035c09bffe7eeedf21c8ebbdde3c96c0f53badf5
Celaleddin Hidayetoglu <celaleddin.hidayetoglu@gmail.com>
2024-12-15 19:06:57 +0400

    Add day #10 part two

--- day_10.py
+++ day_10.py
@@ -28,7 +28,7 @@ def follow_trail(map: Map, position: Position, previous_height: int, on_trail_en
     follow_trail(map, (x+1, y), current_height, on_trail_end)
     return
 
-def get_trailhead_score(map: Map, trailhead_position):
+def evaluate_trailhead_score(map: Map, trailhead_position: Position) -> int:
     found_trail_ends = dict()
 
     def on_trail_end(p):
@@ -37,15 +37,32 @@ def get_trailhead_score(map: Map, trailhead_position):
     follow_trail(map, trailhead_position, -1, on_trail_end)
     return len(found_trail_ends)
 
-def solve_part_1(map: Map):
+def evaluate_trailhead_rating(map: Map, trailhead_position: Position) -> int:
+    rating = 0
+
+    def on_trail_end(p):
+        nonlocal rating
+        rating += 1
+
+    follow_trail(map, trailhead_position, -1, on_trail_end)
+    return rating
+
+def get_total_trailhead_point(map: Map, evaluation_function: Callable) -> int:
     return sum(
-        get_trailhead_score(map, (x, y))
+        evaluation_function(map, (x, y))
         for x in range(len(map))
         for y in range(len(map[0]))
         if map[x][y] == 0
     )
 
+def solve_part_1(map: Map):
+    return get_total_trailhead_point(map, evaluate_trailhead_score)
+
+def solve_part_2(map: Map):
+    return get_total_trailhead_point(map, evaluate_trailhead_rating)
+
 if __name__ == '__main__':
     map = read_map()
 
     print(f"Answer for part 1: {solve_part_1(map)}")
+    print(f"Answer for part 1: {solve_part_2(map)}")
